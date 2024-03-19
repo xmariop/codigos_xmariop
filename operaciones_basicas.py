6ad85956ecab8ec6de0c2f5c3739f2989575222a
xmariop <164095623+xmariop@users.noreply.github.com>
2024-03-19 17:18:33 -0500

    Create operaciones_basicas.py

---
+++ operaciones_basicas.py
@@ -0,0 +1,11 @@
+#Operaciones Básicas
+a = float(input("Digita un numero: "))
+b = float(input("Digita otro numero: "))
+suma=a+b
+resta = a-b
+prod=a*b
+divi=a/b
+print (a, "+", b, "=", suma)
+print (a, "-", b, "=", resta)
+print (a, "*", b, "=", prod)
+print (a,"/", b, "=", divi)
